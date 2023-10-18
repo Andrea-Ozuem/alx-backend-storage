6295046d2a610e5df80ff2b0b0bdd94700c148d3
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-18 18:35:07 +0100

    tests

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-'Practice redies  0'
+'''Create a Cache class. In the __init__ method, store an instance of the
+Redis client as a private variable named _redis (using redis.Redis())
+and flush the instance using flushdb.
 
-import uuid
+Create a store method that takes a data argument and returns a string.
+The method should generate a random key (e.g. using uuid), store the
+input data in Redis using the random key and return the key.
+'''
+
 import redis
+import uuid import uuid4
 from typing import Union
 
 
@@ -14,7 +21,8 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[int, str, float, bytes]) -> str:
-        '''store'''
-        u_id: str = str(uuid.uuid4())
+        '''generate a random key, use it to fill a value in the
+        database, and return a string'''
+        u_id: str = str(uuid4())
         self._redis.set(u_id, data)
         return u_id
