66fd36bd8f273d7ac10b8168b9d0e4baca9cde6f
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-18 15:01:06 +0100

    fix: code should not be executed when imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,17 +4,18 @@
 
 from pymongo import MongoClient
 
-
-client = MongoClient('mongodb://127.0.0.1:27017')
-log_collection = client.logs.nginx
-print(log_collection.count_documents({}), 'logs')
-print('Methods:')
-print(f'\tmethod GET: {log_collection.count_documents({"method": "GET"})}')
-print(f'\tmethod POST: {log_collection.count_documents({"method": "POST"})}')
-print(f'\tmethod PUT: {log_collection.count_documents({"method": "PUT"})}')
-patch = log_collection.count_documents({"method": "PATCH"})
-print(f'\tmethod PATCH: {patch}')
-delete = log_collection.count_documents({"method": "DELETE"})
-print(f'\tmethod DELETE: {delete}')
-status = log_collection.count_documents({"method": "GET", "path": "/status"})
-print(f'{status} status check')
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_collection = client.logs.nginx
+    print(log_collection.count_documents({}), 'logs')
+    print('Methods:')
+    print(f'\tmethod GET: {log_collection.count_documents({"method": "GET"})}')
+    post = log_collection.count_documents({"method": "POST"})
+    print(f'\tmethod POST: {post}')
+    print(f'\tmethod PUT: {log_collection.count_documents({"method": "PUT"})}')
+    patch = log_collection.count_documents({"method": "PATCH"})
+    print(f'\tmethod PATCH: {patch}')
+    delete = log_collection.count_documents({"method": "DELETE"})
+    print(f'\tmethod DELETE: {delete}')
+    stat = log_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f'{stat} status check')
